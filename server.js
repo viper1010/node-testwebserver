bcb9f906ea9a1e2396551dad7f11d538cd3f5869
viper1010 <viper.lannister@gmail.com>
2017-09-11 19:22:35 -0400

    Adding Start Script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -57,6 +58,6 @@ app.get('/api/user', (req, res)=>{
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
 });
